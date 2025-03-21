d9dcab34e395821dfb13bb162c3713db81cbd3fd
Dhivyasathishkumar <dhivyathiruvenkadam@gmail.com>
2025-03-21 22:57:12 +0530

    revised buttonanimations and infinitescroll

--- buttontransition/server.js
+++ buttontransition/server.js
@@ -1,24 +1,27 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
-// Serve static files like HTML, CSS, JS
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Set up a mock API endpoint to return animation types
 app.get('/api/animation', (req, res) => {
     console.log('API Request received', req.query);
     const elementId = req.query['ic-element-id'];
-
     // const animations = ['bounce', 'shake', 'fade', 'zoom'];
     console.log("random animation choosen", elementId);
     if (elementId === "bounceBtn") 
       res.status(200).send("Bounce Off");
     else if (elementId === "shakeBtn")
       res.status(200).send("I am shaking!");
+     else if (elementId === "zoomBtn")
+      res.status(200).send("I am zooming!");
+     else if (elementId === "flipBtn")
+      res.status(200).send("Flip Animation Triggered!");
+     else if (elementId === "spinBtn")
+        res.status(200).send("Spinning Around!");
+    else if (elementId === "fadeBtn")
+        res.status(200).send("Fading In and Out!");
+    else
+      res.status(400).send("Unknown animation ID");
 });
-
-// Start the server on port 3000
 app.listen(3000, () => {
     console.log('Server running on http://127.0.0.1:3000');
 });
